74525b3f61243a80ec48ed58079d11cb506a2f4d
mCloud-Platform <mcloudautomation@gmail.com>
2022-08-26 21:40:50 +0530

    test

--- uptime/health-check.sh
+++ uptime/health-check.sh
@@ -25,44 +25,49 @@ echo "***********************"
 echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
 
 mkdir -p logs
-
-for (( index=0; index < ${#KEYSARRAY[@]}; index++))
-do
-  key="${KEYSARRAY[index]}"
-  url="${URLSARRAY[index]}"
-  echo "  $key=$url"
-
-  for i in 1 2 3 4;
+for (( i = 0; i < ${1:-10}; i++ )); do
+  for (( index=0; index < ${#KEYSARRAY[@]}; index++))
   do
-    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
-    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
-      result="success"
+    key="${KEYSARRAY[index]}"
+    url="${URLSARRAY[index]}"
+    echo "  $key=$url"
+
+    for i in 1 2 3 4;
+    do
+      response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+      if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
+        result="success"
+      else
+        result="failed"
+      fi
+      if [ "$result" = "success" ]; then
+        break
+      fi
+      sleep 5
+    done
+    dateTime=$(date +'%Y-%m-%d %H:%M')
+    if [[ $commit == true ]]
+    then
+      echo $dateTime, $result >> "logs/${key}_report.log"
+
+      # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
+      echo "$(tail -8000 logs/${key}_report.log)" > "logs/${key}_report.log"
     else
-      result="failed"
+      echo "    $dateTime, $result"
     fi
-    if [ "$result" = "success" ]; then
-      break
-    fi
-    sleep 5
   done
-  dateTime=$(date +'%Y-%m-%d %H:%M')
+
   if [[ $commit == true ]]
   then
-    echo $dateTime, $result >> "logs/${key}_report.log"
-
-    # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
-    echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
-  else
-    echo "    $dateTime, $result"
+    # Let's make mCloudAutomation the most productive person on GitHub.
+    git pull
+    git config --global user.name 'mCloud-Platform'
+    git config --global user.email 'mCloudAutomation@gmail.com'
+    git add -A --force logs/
+    git commit -am '[Automated] Update Health Check Logs'
+    git push
   fi
-done
 
-if [[ $commit == true ]]
-then
-  # Let's make mCloudAutomation the most productive person on GitHub.
-  git config --global user.name 'mCloud-Platform'
-  git config --global user.email 'mCloudAutomation@gmail.com'
-  git add -A --force logs/
-  git commit -am '[Automated] Update Health Check Logs'
-  git push
-fi
+  echo "we will check again in few seconds."
+  sleep 180
+done
